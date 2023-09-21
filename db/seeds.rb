c73b238f163116ed58b122baea471984c71d489c
SaneMyburg <sane.myburg@gmail.com>
2023-09-21 08:00:48 +0200

    Generate api for messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+custom_messages = [
+  "Hello, how are you today?",
+  "Welcome to our website!",
+  "Thanks for visiting!",
+  "Have a great day!",
+  "We hope you enjoy your stay!"
+]
+
+custom_messages.each do |message|
+  Message.create(text: message)
+end
